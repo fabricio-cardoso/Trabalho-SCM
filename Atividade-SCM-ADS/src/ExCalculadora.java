279bbf2e5223d77a6094f1f659b4552291c42216
Fabricio Cardoso <fabriciocardosom@gmail.com>
2017-04-18 15:56:05 -0300

    interface de menu adicionada

--- Atividade-SCM-ADS/src/ExCalculadora.java
+++ Atividade-SCM-ADS/src/ExCalculadora.java
@@ -1,3 +1,4 @@
+
 /**
  * 
  */
@@ -13,9 +14,23 @@ public class ExCalculadora {
 	 */
 	public static void main(String[] args) {
 		Calculadora c1 = new Calculadora();
-		c1.num1 = c1.lerInteiro("Digite um inteiro: ");
-		c1.num2 = c1.lerInteiro("Digite outro inteiro");
 		
-		System.out.println(c1.somar(c1.num1, c1.num2));
+	
+		do{
+			c1.num1 = c1.lerInteiro("Digite um inteiro: ");
+			c1.num2 = c1.lerInteiro("Digite outro inteiro");
+			c1.menu();	
+		switch(c1.op){
+				case 1:
+					System.out.println(c1.somar(c1.num1, c1.num2));
+				break;
+		//		default:
+			//		System.out.println("opção inválida");
+		}			
+			}
+		while(c1.op != 0);
+		
+		System.out.println("saiu");
+
 	}
 }
